7bf89db8d82959abedceae401b21dada15498546
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-02-02 00:06:15 +0000

    Major rework

--- WaterMarker.sh
+++ WaterMarker.sh
@@ -5,5 +5,21 @@ mkdir python-virtual-environments
 cd python-virtual-environments
 
 # create virtual environment
-python3 -m venv env
+python3 -m venv wm-env
 
+# activate virtual environment
+source wm-env/bin/activate
+
+# install requirements
+python3 pip install pillow
+
+# run the script
+python3 WaterMarker.py
+
+sleep 30m
+
+# deactivate virtual environment
+deactivate
+
+# clean up
+rm -rf wm-env
