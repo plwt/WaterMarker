806359df5902756340f279c2487b13f057789cb0
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-04-10 15:27:40 +0100

    Update WaterMarker.sh

--- WaterMarker.sh
+++ WaterMarker.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# create folder for virtual environment
-mkdir python-virtual-environments
-
-# go to the folder
-cd python-virtual-environments
-
-# create virtual environment
-python3 -m venv wm-env
-
-# activate virtual environment
-source wm-env/bin/activate
-
 # install requirements
 python3 pip install pillow
 
@@ -19,9 +7,3 @@ python3 pip install pillow
 python3 WaterMarker.py
 
 sleep 30m
-
-# deactivate virtual environment
-deactivate
-
-# clean up
-rm -rf wm-env
