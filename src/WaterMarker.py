3f65264931334b1871f38486e49db3bcc7c809bb
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-04-12 15:15:04 +0100

    Update WaterMarker.py

--- src/WaterMarker.py
+++ src/WaterMarker.py
@@ -1,6 +1,6 @@
 # install pillow
 
-from Pillow import Image
+from PIL import Image
 
 print("Welcome to WaterMarker")
 print("Have you saved the file to be watermarked to the WaterMarker folder with filename image.png?(y/n)")
@@ -12,13 +12,14 @@ while Gonogo=="n":
     Gonogo=input()  
 
 if Gonogo=="y":
-    baseimage=Image.open('/home/WaterMarker/image.png')
-    baseimage.putalpha(100)
-    hmat,wmat = baseimage.size
-    watermark=Image.open('/opt/WaterMarker/src/watermark.png')
-    complete=watermark.resize((hmat,wmat))
-    complete.paste(baseimage,box=(0,0),watermark=baseimage)
-    complete.save("/home/WaterMarker/watermarkedimage.png")
+    words=Image.open('/opt/WaterMarker/src/home/WaterMark/image.png')
+    words.putalpha(50)
+    hmat,wmat = words.size
+    mask=Image.open('/opt/WaterMarker/src/watermark.png')
+    mask.putalpha(100)
+    complete=mask.resize((hmat,wmat))
+    complete.paste(words,box=(0,0),mask=words)
+    complete.save("/opt/WaterMarker/src/home/WaterMark/watermarkedimage.png")
     print("Your watermarked file has been saved to your desktop with filename watermarkedimage.png")
 else:
     print("Thank you for using WaterMarker.")
