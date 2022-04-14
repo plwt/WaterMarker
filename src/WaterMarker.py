f6d6b36b494ab745d6e1d4a988be6bc27cb908b7
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-04-14 21:46:34 +0100

    Update WaterMarker.py

--- src/WaterMarker.py
+++ src/WaterMarker.py
@@ -8,18 +8,18 @@ print("Have you saved the file to be watermarked to the WaterMarker folder with 
 Gonogo=input()
 
 while Gonogo=="n":
-    print("Have you saved the file to be watermarked to the WaterMarker folder with filename image.png?(y/n)")
+    print("Have you saved the file to be watermarked to the WaterMarker folder (/opt/WaterMarker/src/images) with filename image.png?(y/n)")
     Gonogo=input()  
 
 if Gonogo=="y":
-    words=Image.open('/home/WaterMarkerimages/image.png')
+    words=Image.open('/opt/WaterMarker/images/image.png')
     # words.putalpha(255)
     hmat,wmat = words.size
     mask=Image.open('/opt/WaterMarker/src/watermark.png')
     mask.putalpha(150)
     complete=mask.resize((hmat,wmat))
     complete.paste(words,box=(0,0),mask=words)
-    complete.save("/home/WaterMarkerimages/watermarkedimage.png")
-    print("Your watermarked file has been saved to /home/WaterMarkerimages/watermarkedimage.png")
+    complete.save("/opt/WaterMarker/images/watermarkedimage.png")
+    print("Your watermarked file has been saved to /opt/WaterMarker/images/watermarkedimage.png")
 else:
     print("Thank you for using WaterMarker.")
