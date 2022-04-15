5d84399f22de5c7390aaa5194cc5d21ddd038144
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-04-15 22:12:08 +0100

    Re-labelled

--- src/WaterMarker.py
+++ src/WaterMarker.py
@@ -4,28 +4,30 @@ from PIL import Image
 
 import OS
 
+print(" ")
 print("Welcome to WaterMarker")
-print("Have you saved the file to be watermarked to the WaterMarker folder with filename image.png?(y/n)")
+print(" ")
 
-Gonogo=input()
+Gonogo=input("Have you saved the file to be watermarked to the WaterMarker folder with filename image.png?(y/n)")
 
-while Gonogo=="n":
-    print("Have you saved the file to be watermarked to the WaterMarker folder (/opt/WaterMarker/src/images) with filename image.png?(y/n)")
-    Gonogo=input()  
+if Gonogo=="n":
+    print("Please save the image to be watermarked to /opt/WaterMarker/images with the filename image.jpg and run WaterMarker again.")
+    break  
 
-if Gonogo=="y":
-    words=Image.open('/opt/WaterMarker/images/image.jpg')
-    words.save("/opt/WaterMarker/images/temp.png")
-    words2=Image.open("/opt/WaterMarker/images/temp.png")
-    words2.putalpha(250)
-    hmat,wmat = words2.size
-    mask=Image.open('/opt/WaterMarker/src/watermark.png')
-    mask.putalpha(255)
-    complete=mask.resize((hmat,wmat))
-    complete.paste(words2,box=(0,0),mask=words2)
+elif Gonogo=="y":
+    original=Image.open('/opt/WaterMarker/images/image.jpg')
+    original.save("/opt/WaterMarker/images/temp.png")
+    original2=Image.open("/opt/WaterMarker/images/temp.png")
+    original2.putalpha(250)
+    hmat,wmat = original2.size
+    watermark=Image.open('/opt/WaterMarker/src/watermark.png')
+    watermark.putalpha(255)
+    complete=watermark.resize((hmat,wmat))
+    complete.paste(original2,box=(0,0),watermark=original2)
     complete.save("/opt/WaterMarker/images/watermarkedimage.png")
     path = "/opt/WaterMarker/images/temp.png"
     os.remove("/opt/WaterMarker/images/temp.png")
     print("Your watermarked file has been saved to /opt/WaterMarker/images/watermarkedimage.png")
 else:
     print("Thank you for using WaterMarker.")
+    break
