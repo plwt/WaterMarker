aee61b960060a60d806bd93229ba40ac6b7a40d0
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-04-16 21:05:47 +0100

    Major update to allow for custom watermarks.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ An image watermarking tool.
 
 To do: tidy up the file paths and run further tests
 
-Next: make it easy for a user to customise the watermark
+Next: comment code
--- src/WaterMarker.py
+++ src/WaterMarker.py
@@ -1,32 +1,60 @@
 # install pillow
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 import OS
 
-print(" ")
-print("Welcome to WaterMarker")
-print(" ")
+def welcome():
+    print("""
+    Welcome to WaterMarker
+    """)
 
-Gonogo=input("Have you saved the file to be watermarked to the WaterMarker folder with filename image.png?(y/n)")
+welcome()
 
-if Gonogo=="n":
-    print("Please save the image to be watermarked to /opt/WaterMarker/images with the filename image.jpg and run WaterMarker again.")
-    break  
 
-elif Gonogo=="y":
-    original=Image.open('/opt/WaterMarker/images/image.jpg')
-    original.save("/opt/WaterMarker/images/temp.png")
-    original2=Image.open("/opt/WaterMarker/images/temp.png")
-    original2.putalpha(250)
-    hmat,wmat = original2.size
-    watermark=Image.open('/opt/WaterMarker/src/watermark.png')
-    watermark.putalpha(255)
-    complete=watermark.resize((hmat,wmat))
-    complete.paste(original2,box=(0,0),watermark=original2)
-    complete.save("/opt/WaterMarker/images/watermarkedimage.png")
-    path = "/opt/WaterMarker/images/temp.png"
-    os.remove("/opt/WaterMarker/images/temp.png")
-    print("Your watermarked file has been saved to /opt/WaterMarker/images/watermarkedimage.png")
-else:
-    print("Thank you for using WaterMarker.")
+
+def make_watermark():
+
+    width = 512
+    height = 512
+    message = input("Enter your message: ")
+    font = ImageFont.truetype("arial.ttf", size=20)
+    img = Image.new('RGB', (width, height), color='blue')
+    imgDraw = ImageDraw.Draw(img)
+    textWidth, textHeight = imgDraw.textsize(message, font=font)
+    xText = (width - textWidth) / 2
+    yText = (height - textHeight) / 2
+
+    imgDraw.text((xText, yText), message, font=font, fill=(255, 255, 0))
+
+    img.save('/opt/WaterMarker/src/watermark.png')
+
+make_watermark()
+
+
+
+def watermarker():
+    Gonogo=input("Have you saved the file to be watermarked to the the WaterMarker folder in your user documents with filename image.png?(y/n)")
+    
+    if Gonogo=="n":
+        print("Please save the image to be watermarked to the WaterMarker folder in your user documents with the filename image.jpg and run WaterMarker again.")
+        break  
+
+    elif Gonogo=="y":
+        original=Image.open('/opt/WaterMarker/images/image.jpg')
+        original.save("/opt/WaterMarker/images/temp.png")
+        original2=Image.open("/opt/WaterMarker/images/temp.png")
+        original2.putalpha(250)
+        hmat,wmat = original2.size
+        watermark=Image.open('/opt/WaterMarker/src/watermark.png')
+        watermark.putalpha(255)
+        complete=watermark.resize((hmat,wmat))
+        complete.paste(original2,box=(0,0),watermark=original2)
+        complete.save("/opt/WaterMarker/images/watermarkedimage.png")
+        path = "/opt/WaterMarker/images/temp.png"
+        os.remove("/opt/WaterMarker/images/temp.png")
+        print("Your watermarked file has been saved to the WaterMarker folder in your user documents with the filename watermarkedimage.png")
+    else:
+        print("Thank you for using WaterMarker.")
+
+watermarker()
