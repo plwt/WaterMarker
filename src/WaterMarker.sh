309dbd2e252adbc7eddf56beac067416dc26d5b0
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-04-19 15:25:53 +0100

    Update WaterMarker.sh

--- src/WaterMarker.sh
+++ src/WaterMarker.sh
@@ -11,13 +11,16 @@ python3 -m pip install --upgrade Pillow
 
 # make folder
 mkdir -p ~/WaterMarker
-mkdir /opt/WaterMarker/Images
+mkdir /opt/WaterMarker/images
 
 # symbolic link
 ln -s /opt/WaterMarker/images ~/WaterMarker/
 
 # run the script
 python3 /opt/WaterMarker/src/WaterMarker.py
+
+# copy file 
+cp ~/WaterMarker/image.jpg /opt/WaterMarker/images/image.jpg
 
 sleep 30m
 
